8e654b79f587ab7c296f90ec0a24e517eab95890
prestonharold01 <preston.harold22@ycdsbk12.ca>
2019-10-01 09:09:26 -0400

    new live hack files

---
+++ Working/PracticeQuestions/1_1_3operaters.py
@@ -0,0 +1,3 @@
+name= "Preston"
+age= 15
+print("Hi! I go by", str(name) + " and I am", str(age)  + " years old")
---
+++ Working/PracticeQuestions/1_2_4_operators.py
@@ -0,0 +1,6 @@
+length= 10
+width= 5
+area= (length * width )
+print(area)
+perimeter= length*2 + width*2
+print( perimeter)
